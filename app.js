997fb473d0fd1f39ce44c518134b78d55c1d2c82
digitalfreelanceruk <andrew@digitalfreelancer.co.uk>
2015-04-29 08:17:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,98 @@ angular.module('scheduleApp', ['firebase'])
   var fb = $firebase(ref);
     
     // sync as object 
-    var syncObject = fb.$asObject();
-    
-    // three way data binding
-    syncObject.$bindTo($scope, 'days');
+  $scope.days = $firebaseObject(ref);
+  
+  $scope.reset = function() {    
+    ref.$set({
+      monday: {
+        name: 'Monday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
+      },
+      tuesday: {
+        name: 'Tuesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	  }
+      },
+      wednesday: {
+        name: 'Wednesday',
+        slots: {
+          900: {
+            time: '9:00am',
+            booked: false
+          },
+          0110: {
+            time: '11:00am',
+            booked: false
+          },
+          100: {
+            time: '1:00pm',
+            booked: false
+          },
+          300: {
+            time: '3:00pm',
+            booked: false
+          },
+          500: {
+            time: '5:00pm',
+            booked: false
+          },
+          700: {
+            time: '7:00pm',
+            booked: false
+          }
+    	}
+      }
+    });    
+  };
   
 });
