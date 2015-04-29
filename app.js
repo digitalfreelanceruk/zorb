71f3f9fc93e24bc40dfc947d8177e77aa7eacb55
digitalfreelanceruk <andrew@digitalfreelancer.co.uk>
2015-04-29 08:11:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,40 +7,6 @@ angular.module('scheduleApp', ['firebase'])
   // connect to firebase 
   var ref = new Firebase("https://incandescent-heat-8927.firebaseio.com/days");  
   var fb = $firebase(ref);
-  
-    // function to set the default data
-    $scope.reset = function() {    
-  
-      fb.$set({
-        monday: {
-          name: 'Monday',
-          slots: {
-            0900: {
-              time: '9:00am',
-              booked: false
-            },
-            0110: {
-              time: '11:00am',
-              booked: false
-            }
-          }
-        },
-        tuesday: {
-          name: 'Tuesday',
-          slots: {
-            0900: {
-              time: '9:00am',
-              booked: false
-            },
-            0110: {
-              time: '11:00am',
-              booked: false
-            }
-          }
-        }
-      });    
-  
-    };
     
     // sync as object 
     var syncObject = fb.$asObject();
